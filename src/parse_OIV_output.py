e895ea3eff6c13d590f960c3d1c3ff11a8eb4e54
Jamey Hanson <jphanso@evoforge.org>
2017-09-19 12:19:33 -0400

    converting to list

--- src/parse_OIV_output.py
+++ src/parse_OIV_output.py
@@ -10,15 +10,15 @@ ignore all other root tags
 
 directory root
     parentID and (0 to many) child IDs
-    output_opt1:
+    flat_output:
         parentID1, childID1
         parentID1, childID2
         parentID2, childID1
-    output_opt2:
+    list_output:
         {parentID1:[childID1, childID2]}
         {parentID2:[chileID2]}
 inode root
-    ID and attributes (type, name, 
+    ID and attributes (type, name, mtime, atime, numBytes)
 
 '''
 
@@ -27,13 +27,18 @@ from xml.etree.ElementTree import ParseError
 from sys import exit
 from tkinter import _support_default_root
 
-def shred_line(line):
-    def shred_directory(root):
-        print(root.tag)
+def shred_line(line, output_flat):
+    def shred_directory(root, output_flat):
+        # print(root.tag)
+        jamey = list(root) # convert iterable to list
+        print(jamey[0].text)
         for elem in root:
-            print(elem.text)
+            if output_flat:
+                print(elem.text)
+            else:
+                print(elem.txt)
     
-    def shred_inode(root):
+    def shred_inode(root, output_flat):
         print(root.tag)
         for elem in root:
             if elem.text != None:
@@ -47,10 +52,10 @@ def shred_line(line):
     try:
         root = ET.fromstring(line)
         
-        if root.tag == 'directory':
-            shred_directory(root)
-        if root.tag == 'inode':
-            shred_inode(root)
+        if root.tag == 'directory': # parent, child by ID
+            shred_directory(root, output_flat)
+        if root.tag == 'inode': # attributes, including name
+            shred_inode(root, output_flat)
         else:
             pass
     except ParseError as e:
@@ -67,9 +72,11 @@ def shred_line(line):
 #             print(e)   
 
 def main():
+    OUTPUT_FLAT = True
     
-    line = '<inode><id>16432</id><type>FILE</type><name>apache-log4j-extras-1.2.17.jar</name><replication>3</replication><mtime>1504280150137</mtime><atime>1504280149965</atime><preferredBlockSize>134217728</preferredBlockSize><permission>oozie:oozie:0775</permission><blocks><block><id>1073741834</id><genstamp>1010</genstamp><numBytes>448794</numBytes></block></blocks><storagePolicyId>0</storagePolicyId></inode>'
+#    line = '<inode><id>16432</id><type>FILE</type><name>apache-log4j-extras-1.2.17.jar</name><replication>3</replication><mtime>1504280150137</mtime><atime>1504280149965</atime><preferredBlockSize>134217728</preferredBlockSize><permission>oozie:oozie:0775</permission><blocks><block><id>1073741834</id><genstamp>1010</genstamp><numBytes>448794</numBytes></block></blocks><storagePolicyId>0</storagePolicyId></inode>'
 #    line = '<directory><parent>16390</parent><child>16392</child><child>16393</child></directory>'
+    line = '<directory><parent>16428</parent><child>16429</child><child>16431</child><child>16539</child><child>16654</child><child>16781</child><child>16783</child><child>16791</child><child>16859</child><child>16860</child><child>17042</child></directory>'
 #     line = '<INodeDirectorySection><directory><parent>16385</parent><child>17451</child><child>16386</child><child>16389</child></directory>'
 #     line = '</INodeDirectorySection>'
 #     line = '<FileUnderConstructionSection></FileUnderConstructionSection>'
@@ -77,7 +84,7 @@ def main():
 #     line = '<lastInodeId>19541</lastInodeId><numInodes>2540</numInodes><inode><id>16385</id><type>DIRECTORY</type><name></name><mtime>1504282402845</mtime><permission>hdfs:supergroup:0755</permission><nsquota>9223372036854775807</nsquota><dsquota>-1</dsquota></inode>'
 #     line = '<SecretManagerSection><currentId>0</currentId><tokenSequenceNumber>0</tokenSequenceNumber><numDelegationKeys>0</numDelegationKeys><numTokens>0</numTokens></SecretManagerSection><CacheManagerSection><nextDirectiveId>1</nextDirectiveId><numDirectives>0</numDirectives><numPools>0</numPools></CacheManagerSection>'
 #     line = 'fsdafdsfdsa'
-    shred_line(line)
+    shred_line(line, OUTPUT_FLAT)
 
 if __name__ == '__main__':
     main()
